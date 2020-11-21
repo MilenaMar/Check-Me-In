b2561ac39e3be985c3cc9b03ae77aec34af12a38
Ana Milena Martinez Caceres <67890212+MilenaMar@users.noreply.github.com>
2020-11-21 18:11:26 +0000

    User can Post now

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,37 @@ res.render('user/settings',{ currentUser: req.session.user })})
 router.get('/newpost',isLoggedIn,(req,res)=> { 
 res.render('user/new-post',{ currentUser: req.session.user })})
 
+
+router.post("/newpost",isLoggedIn, (req, res) => {
+  const { country,city,budget,currency,days,when,description, body } = req.body;
+  Post.create({
+    type: "text",
+    country,
+    city,
+    budget,
+    currency,
+    days,
+    when,
+    description,
+    body,
+    author: req.session.user._id,
+  }).then((createdPost) => {
+    // after you create a post, the property author was added to it, but the user is not aware of that, so we must edit the user and the post to the user's posts array
+    console.log("createdPost:", createdPost);
+    User.findByIdAndUpdate(
+      req.session.user._id,
+      {
+        $addToSet: { posts: createdPost._id },
+      },
+      { new: true }
+    ).then((newAndUpdatedUser) => {
+      console.log("newAndUpdatedUser:", newAndUpdatedUser);
+      res.redirect("/user/profile");
+    });
+  });
+});
+
+
 router.get('/readmore',isLoggedIn,(req,res)=> { 
   res.render('user/read-more',{ currentUser: req.session.user })})
 module.exports = router;
